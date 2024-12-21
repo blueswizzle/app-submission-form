9f36fc37e32244ee1f127158b6e229a853ed4fef
Atheek Ashkar <aashkar2017@fau.edu>
2024-12-21 08:57:10 -0500

    Minor edits to border styling on navbar

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <nav className='flex flex-col items-center border border-b-2'>
+            <nav className='flex flex-col items-center border-b-2'>
                 <Image src='/Creed Logo.png' width={200} height={200} alt='logo' />
                 <h1 className='font-bold'>Commerce Platform & Global Business Directory</h1>
                 <h2>Empowering, Engaging and Serving the Ummah</h2>
