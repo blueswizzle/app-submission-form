74fdd1cbcc5f74bfc5ae8f11c952cc051f5c20a8
Atheek Ashkar <aashkar2017@fau.edu>
2024-12-21 08:55:02 -0500

    Added navbar component and updated favicon

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,8 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
     return (
         <>
+            <nav className='flex flex-col items-center border border-b-2'>
+                <Image src='/Creed Logo.png' width={200} height={200} alt='logo' />
+                <h1 className='font-bold'>Commerce Platform & Global Business Directory</h1>
+                <h2>Empowering, Engaging and Serving the Ummah</h2>
+            </nav>
 
 
         </>
