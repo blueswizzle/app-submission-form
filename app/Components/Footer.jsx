5361a87d7762094710b11db70b4608792fd014f2
Atheek Ashkar <aashkar2017@fau.edu>
2024-12-21 09:45:22 -0500

    Created the footer component

---
+++ app/Components/Footer.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className='flex flex-col items-center justify-center'>
+                <div>
+                    <Link href='/' className='text-xl'>Home</Link>
+                    <span className='text-gray-400 text-2xl font-medium'> | </span>
+                    <Link href='/about' className='text-xl'>About Us</Link>
+                </div>
+
+                <p className='text-center'>
+                    Copyright &copy; {new Date().getFullYear()} OUR CREED - All rights reserved.
+                </p>
+
+            </footer>
+        </>
+    )
+}
+
+export default Footer
